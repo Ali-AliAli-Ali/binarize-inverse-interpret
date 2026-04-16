11c12cc22982e44a7f421a0f2bcd840528dc16b0
Ali-AliAli-Ali <alivolf@icloud.com>
2026-04-16 12:56:21 +0300

    Added space for uncorrelated fc features analysis, moved helpful functions to other dir, added local logs to .gitignore

---
+++ common_key_functions/all_plots.py
@@ -0,0 +1,125 @@
+import os
+import numpy as np
+from typing import Optional, List, Tuple
+from matplotlib import pyplot as plt, colors, cm
+
+
+def plot_metric(trains: List, 
+                tests: List, 
+                epochs: int, 
+                model_name: Optional[str] = "model",
+                dataset_name: Optional[str] = "dataset",
+                metric_name: Optional[str] = "metric",
+                figsize: Optional[Tuple] = (25, 3),
+                save_graph: Optional[bool] = True,
+                graph_dir: Optional[str] = "graphs"):
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(
+        [i+1 for i in range(epochs)],
+        trains, 
+        "b.-", alpha=0.6, lw=0.3, ms=0.8,
+        label="train " + metric_name
+    )
+    ax.plot(
+        [i+1 for i in range(epochs)],
+        tests, 
+        "r.-", alpha=0.8, lw=0.3,
+        label="test " + metric_name
+    )
+    ax.set(
+        title=f"{model_name} {metric_name} on {dataset_name} for {epochs} epochs",
+        xlabel="epochs", xlim=(-1, epochs+1), xticks=[i for i in range(0, epochs+1, 5)],
+        ylabel=metric_name
+    )
+    ax.grid(True, alpha=0.3, zorder=0)
+    ax.legend()
+
+    if save_graph:
+        plt.savefig(os.path.join(graph_dir, f"regnet_{metric_name}_{epochs}epochs.png"))
+    
+
+def centralize_colormap(values, 
+                        center: Optional[float] = 0):
+    values_min = np.min(values)
+    values_max = np.max(values)
+    border = max(abs(values_min), abs(values_max))
+    
+    return colors.TwoSlopeNorm(
+        vmin=-border,
+        vcenter=center, 
+        vmax=border
+    )
+
+
+def plot_heatmap(values,
+                 cmap_name: Optional[str] = "seismic",
+                 title: Optional[str] = "Weights heatmap",
+                 figsize: Optional[Tuple] = (25, 3),
+                 save_graph: Optional[bool] = True,
+                 graph_dir: Optional[str] = "graphs"):
+    plt.figure(figsize=figsize)
+    plt.imshow(
+        values, 
+        origin="lower",
+        cmap=cmap_name, 
+        norm=centralize_colormap(values),
+        extent=(0, len(values[0]) + 1, 0, len(values) + 1)
+    )  
+    plt.title(title)  
+    plt.colorbar()  
+
+    if save_graph:
+        plt.savefig(os.path.join(graph_dir, f"regnet_{title}.png"))
+
+
+def plot_colored_barplot(values,
+                         values_color,
+                         values_color_name: str,
+                         cmap_name: Optional[str] = "seismic",
+                         edge_color: Optional[str] = "black",
+                         title: Optional[str] = "Values barplot",
+                         xlabel: Optional[str] = "values",
+                         ylabel: Optional[str] = "values number",
+                         xticks: Optional[List] = [],
+                         xticklabels: Optional[List] = [],   
+                         figsize: Optional[Tuple] = (25, 3),
+                         save_graph: Optional[bool] = True,
+                         graph_dir: Optional[str] = "graphs"):
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    values_cmap = plt.get_cmap(cmap_name)
+    norm = centralize_colormap(values_color)
+    ax.bar(
+        [i for i in range(len(values))],
+        values, 
+        color=values_cmap(norm(values_color)),
+        edgecolor=edge_color
+    )
+    
+    scalar_map = cm.ScalarMappable(
+        cmap=values_cmap, 
+        norm=norm
+    )
+    scalar_map.set_array([])
+    colorbar = plt.colorbar(scalar_map, ax=ax)
+    colorbar.set_label(values_color_name)
+    
+    if len(xticks):
+        ax.set(
+            title=title,
+            xlabel=xlabel,
+            xticks=xticks,
+            ylabel=ylabel
+        )
+        ax.set_xticklabels(xticklabels, rotation=-45)
+    else:
+        ax.set(
+            title=title,
+            xlabel=xlabel,
+            ylabel=ylabel
+        )
+    plt.grid(True, alpha=0.3, zorder=0)
+    plt.tight_layout()
+
+    if save_graph:
+        plt.savefig(os.path.join(graph_dir, f"regnet_{title}.png"))
---
+++ common_key_functions/helpful_funcs.py
@@ -0,0 +1,60 @@
+from time import time
+from typing import Optional
+import torch
+from torch.utils.data import DataLoader
+
+
+seed = 42
+torch.manual_seed(seed)
+
+@torch.inference_mode()
+def validate_top1_top5_time(model, 
+                            loader: DataLoader):
+    device = next(model.parameters()).device
+
+    model.eval()
+    top1_correct = 0
+    top5_correct = 0
+    total = 0
+    
+    start_time = time()
+
+    for images, targets in loader:
+        images = images.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        logits = model(images)
+        _, preds = logits.topk(5, dim=1, largest=True, sorted=True)
+
+        correct = preds.eq(targets.view(-1, 1))
+        top1_correct += correct[:, :1].sum().item()
+        top5_correct += correct.sum().item()
+        total += targets.size(0)
+        
+    val_time = time() - start_time
+
+    return (top1_correct / total * 100,
+            top5_correct / total * 100,
+            val_time)
+    
+def pretty_print_top1_top5_time(model, 
+                                loader: DataLoader, 
+                                top1_orig: Optional[float] = 0,
+                                top5_orig: Optional[float] = 0):    
+    top1_new, top5_new, val_time = validate_top1_top5_time(model, loader)
+    
+    if top1_orig:
+        drop1 = top1_orig - top1_new
+        drop1_report = f", drop: {drop1:.4f}"
+    else:
+        drop1, drop1_report = None, ""
+    if top5_orig:
+        drop5 = top5_orig - top5_new
+        drop5_report = f", drop: {drop5:.4f}"
+    else:
+        drop5, drop5_report = None, ""
+        
+    print(f"    Top-1 Accuracy:  {top1_new:.4f}{drop1_report}")
+    print(f"    Top-5 Accuracy:  {top5_new:.4f}{drop5_report}")
+    print(f"    Validation time: {val_time:.4f} s")
+    return top1_new, top5_new, val_time, drop1, drop5
